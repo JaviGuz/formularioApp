ee23c3072c0ed5585d5e9436fda68c440bc5d4b4
JaviGuz <javier.guzman@feverup.com>
2019-03-10 21:39:10 +0100

    Use ngModel to connect the template and component

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { getMaxListeners } from 'cluster';
 
 @Component({
   selector: 'app-template',
@@ -7,11 +9,21 @@ import { Component } from '@angular/core';
 })
 export class TemplateComponent {
 
+  usuario: Object = {
+    nombre: 'Javier',
+    apellido: 'Guzman',
+    correo: 'xavigu@gmail.com'
+
+
+  };
+
   constructor() { }
 
 
-  guardar() {
-  console.log('Formulario rellenado');
+  guardar(forma: NgForm) {
+     console.log('ngForm: ', forma);
+     console.log('valores de campos: ', forma.value);
+     console.log('datos usuario object: ', this.usuario);
   }
 
 }
