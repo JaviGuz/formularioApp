ae21aa347981c49d47244ae1c7b96272a43aac77
JaviGuz <javier.guzman@feverup.com>
2019-03-31 20:54:32 +0200

    Seteo de valores de usuario y reset form al enviar

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -11,23 +11,46 @@ export class DataComponent  {
 
   forma: FormGroup;
 
+  usuario: Object = {
+
+    nombreCompleto: {
+      nombre: 'Javier',
+      apellido: 'Guzman'
+    },
+
+    correo: 'javier.guzman@gmail.com'
+  };
+
   constructor() {
-    this.forma = new FormGroup({   // hay que pasar un objeto con cierta estructura
-      'nombre': new FormControl('', [
-                                      Validators.required,
-                                      Validators.minLength(3)
-                                    ]),
-      'apellido': new FormControl('', Validators.required),
+
+    console.log(this.usuario.nombreCompleto);
+
+    this.forma = new FormGroup({
+
+      'nombreCompleto': new FormGroup({
+
+        'nombre': new FormControl('', [
+              Validators.required,
+              Validators.minLength(3)
+            ]),
+            'apellido': new FormControl('', Validators.required),
+
+      }),
+
+
       'correo': new FormControl('', [
                                       Validators.required,
                                       Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
                                     ])
     });
 
+    this.forma.setValue(this.usuario); // setea en el formulario el contenido del objeto usuario, siempre que tenga la misma estructura
+
    }
 
    guardarCambios() {
      console.log(this.forma);
+     this.forma.reset(this.usuario); // Una vez guardado el formulario se resetea a sus valores iniciales por defecto
    }
 
 }
